ca3b3c192fca9dde2d42c492f7b229ac091f0aa0
danwadman <109353743+danwadman@users.noreply.github.com>
2022-07-26 13:25:05 +0100

    Small generic updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.js-master/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+  //Back to top button
+
+  //Get the button
+  var mybutton = document.getElementById("backtotop");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+      document.getElementById("backtotop").style.display = "block";
+    } else {
+      document.getElementById("backtotop").style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
