01d3397ab4638e0135571105f7c24dc8db8f080d
danwadman <109353743+danwadman@users.noreply.github.com>
2022-09-26 14:42:58 +0100

    added react features

---
+++ app.js
@@ -0,0 +1,52 @@
+var Morning = React.createClass({
+  render: function() {
+    return (
+      React.createElement('span', { className: 'morning' }, 'Morning! ')
+    );
+  }
+});
+
+var Afternoon = React.createClass({
+  render: function() {
+    return (
+      React.createElement('span', { className: 'afternoon' }, 'Afternoon! ')
+    );
+  }
+});
+
+var Clock = React.createClass({
+  render: function() {
+    return (
+      this.props.hour < 12 ? <Morning /> : <Afternoon /> 
+    );
+  }
+});
+
+var Year = React.createClass({
+  render: function() {
+    return (
+      <p>© Daniel Wadman {new Date().getFullYear()}</p>
+    );
+  }
+})
+
+var hour = (new Date).getHours();
+
+import data from './local.json';
+
+
+
+React.render(
+  React.createElement(Year),
+  document.getElementById('date')
+);
+
+React.render(
+  <Clock hour={hour} />,
+  document.getElementById('clock')
+);
+
+React.render(
+  React.createElement(Year),
+  document.getElementById('date')
+);
